c2aa1ddc4a45947e0e5674f688d88c2686f821e4
Hackk00 <jesuspolo00@gmail.com>
2026-04-10 16:36:01 -0500

    Boton flotante de camara

--- app/src/main/java/com/example/petsocial/FotosActivity.kt
+++ app/src/main/java/com/example/petsocial/FotosActivity.kt
@@ -1,5 +1,21 @@
 package com.example.petsocial
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
-class FotosFragment : Fragment(R.layout.fragment_fotos)
+class FotosFragment : Fragment(R.layout.fragment_fotos){
+
+    private lateinit var fabAgregar: com.google.android.material.floatingactionbutton.FloatingActionButton
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fabAgregar = view.findViewById(R.id.fabAgregar)
+
+        fabAgregar.setOnClickListener {
+            Toast.makeText(requireContext(), "Agregar foto 📸", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
